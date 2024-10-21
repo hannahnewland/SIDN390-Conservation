d6e91f13605bcbe5c4298d3b2cf9585ea3ca1775
hannahnewland <139179132+hannahnewland@users.noreply.github.com>
2024-10-21 18:28:31 +1300

    Adding call to action

--- sketch.js
+++ sketch.js
@@ -9,27 +9,32 @@ var PredatorFreeWellingtonButton;
 var PredatorFreeNZButton;
 var ForestAndBirdButton;
 
+
 function updateCanvasSize(){
 
   screenWidth = window.innerWidth;
   screenHeight = window.innerHeight;
 
   resizeCanvas(screenWidth,screenHeight);
+
 }
 
 function preload(){
 
   BackgroundImage = loadImage('assets/BackgroundImage.jpg');
+  Coolvetica = loadFont('assets/coolvetica.otf');
 }
 
 function setup() {
+
   main_canvas = createCanvas (screenWidth, screenHeight);
   main_canvas.parent('canvasContainer');
   
   // Create the Zealandia Button
   ZealandiaButton = createImg("assets/Zealandia-Logo.png");
-  ZealandiaButton.position(50, screenHeight/3);
+  ZealandiaButton.position(50, screenHeight/2.5);
   ZealandiaButton.size(200,200); // Set the width and height of the button
+  ZealandiaButton.addClass("image-button"); // Add the CSS class to apply styles
   ZealandiaButton.mousePressed(() => {
     window.open("https://www.visitzealandia.com/Get-Involved", "_blank");
   });
@@ -37,8 +42,9 @@ function setup() {
   //Create the Predator Free Wellington Button
 
   PredatorFreeWellingtonButton = createImg("assets/PredatorFreeWellington-Logo.png");
-  PredatorFreeWellingtonButton.position(300,screenHeight/3);
+  PredatorFreeWellingtonButton.position(300,screenHeight/2.5);
   PredatorFreeWellingtonButton.size(200,200);
+  PredatorFreeWellingtonButton.addClass("image-button");
   PredatorFreeWellingtonButton.mousePressed(() => {
 
     window.open("https://www.pfw.org.nz/support-us/", "_blank");
@@ -47,8 +53,9 @@ function setup() {
   //Create the Predator Free NZ Button
 
   PredatorFreeNZButton = createImg("assets/PredatorFreeNZ-Logo.png");
-  PredatorFreeNZButton.position(550,screenHeight/3);
+  PredatorFreeNZButton.position(550,screenHeight/2.5);
   PredatorFreeNZButton.size(200,200);
+  PredatorFreeNZButton.addClass("image-button");
   PredatorFreeNZButton.mousePressed(() => {
     window.open("https://predatorfreenz.org/get-involved/", "_blank");
   });
@@ -56,15 +63,17 @@ function setup() {
   //Create the Forest and Bird Button
 
   ForestAndBirdButton = createImg("assets/ForestAndBird-Logo.png");
-  ForestAndBirdButton.position(800,screenHeight/3);
+  ForestAndBirdButton.position(800,screenHeight/2.5);
   ForestAndBirdButton.size(200,200);
+  ForestAndBirdButton.addClass("image-button");
   ForestAndBirdButton.mousePressed(() => {
     window.open("https://www.forestandbird.org.nz/support-us", "_blank");
   });
 
 
-
 }
+
+
 
 function draw() {
 
@@ -86,9 +95,20 @@ function draw() {
      imgWidth = screenHeight * imgAspectRatio;
    }
  
-  
- 
    image(BackgroundImage, screenWidth/2, screenHeight/2, imgWidth, imgHeight);
+
+   fill('#ffffff');
+   noStroke();
+   rect(0, screenHeight/5.5, screenWidth/1.3, screenHeight/6);
+
+   fill("#009245");
+   textFont(Coolvetica);
+   textSize(screenHeight/20);
+   text('Support the conservation of Kākā in the Wellington Region \nthrough these organisations:', screenWidth/35, screenHeight/4);
+
+
+
+
 }
 
 window.addEventListener('resize',updateCanvasSize);
